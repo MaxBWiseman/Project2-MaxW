83dae0a0744dc7674961e687f825fa062320e493
Max Wiseman <maxwise70@hotmail.co.uk>
2024-06-10 22:56:22 +0000

    feat: Implement game logic for 3x3 grid with reset and score functionality

---
+++ assets/Javascript/script.js
@@ -0,0 +1,33 @@
+const cells = document.getElementsClassName('cell');
+const messageText = document.getElementById('messageText');
+const resetButton = document.getElementById('reset');
+const winningCombos = [ 
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],[0, 3, 6], [1, 4, 7], [2, 5, 8],[0, 4, 8], [2, 4, 6]
+];
+let gameBoard = ['','','','','','','','',''];
+let currentPlayer = 'X';
+let gameActive = false;
+
+function startGame() {
+
+}
+
+function cellClick() {
+
+}
+
+function updateBoard(cell, index) {
+
+}
+
+function changePlayer() {
+
+}
+
+function checkWinner() {
+
+}
+
+function restartGame() {
+
+}   
